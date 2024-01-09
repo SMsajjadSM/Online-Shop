b3986e3d5d032f6a67c286feff547aa4958887ec
sajjad <sajjadsiadati1390@gmail.com>
2024-01-09 19:34:24 +0330

    add tailwin config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
